ef709587ec89d07381834ad7fadf73435e106f77
Gorka <gorka@hey.com>
2023-07-12 19:04:17 -0500

    Add health check endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "/up", to: proc { [200, {}, ["success"]] }
   get "home/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
