d7c02b8904020f9db57e6f7e5d5ed08eafed6fcf
Gorka <gorka@hey.com>
2023-07-12 23:36:48 -0500

    Add helper to render svg

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def svg(name, options = {})
+    file_path = "#{Rails.root}/app/assets/images/svg_icons/#{name}.svg"
+
+    if File.exist? file_path
+      file = File.read(file_path)
+      doc = Nokogiri::HTML::DocumentFragment.parse file
+      svg = doc.at_css "svg"
+      svg["class"] = options[:class] if options[:class].present?
+    else
+      doc = "<!-- SVG #{name} not found -->"
+    end
+
+    raw doc
+  end
 end
