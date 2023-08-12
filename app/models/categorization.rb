4850ff1b4d58cb315ff15f88a5397ebfeb2262f5
Gorka <gorka@hey.com>
2023-08-11 21:58:21 -0500

    Avoid creating duplicated categories

--- app/models/categorization.rb
+++ app/models/categorization.rb
@@ -3,4 +3,8 @@ class Categorization < ApplicationRecord
   belongs_to :categorizable, polymorphic: true
 
   accepts_nested_attributes_for :category
+
+  def category_attributes=(hash)
+    self.category = Category.find_or_create_by(hash)
+  end
 end
